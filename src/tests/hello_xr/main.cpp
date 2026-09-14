6e3e619d4f5353712397322f6746251218c8ad1f
Pipeline V3 <pipeline@igor.local>
2026-09-14 12:21:24 +0200

    feat: connect ArcadeXR audio bridge

--- src/tests/hello_xr/main.cpp
+++ src/tests/hello_xr/main.cpp
@@ -9,6 +9,7 @@
 #include "platformplugin.h"
 #include "graphicsplugin.h"
 #include "mame_boot_probe.h"
+#include "audio_bridge.h"
 #include "openxr_program.h"
 
 #if defined(_WIN32)
@@ -244,6 +245,7 @@ void android_main(struct android_app* app) {
         program->InitializeSession(options->AppSpace);
         program->CreateSwapchains();
         arcadexr::mame::StartBootProbeFromAndroidDebugProperties();
+        arcadexr::audio::Start();
 
         while (app->destroyRequested == 0) {
             // Read all pending events.
